e88242c72f9fdb364bce9952a8cb567437d54e32
DLopez <dlopez@sanagustinnhs.net>
2026-04-27 21:52:01 +0800

    Add new updates to Spoms

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ from datetime import timedelta
 class Config:
     """Flask configuration"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'spoms-secret-key-change-in-production-2026'
-    SESSION_TYPE = os.environ.get('SESSION_TYPE', 'filesystem')
+    SESSION_TYPE = 'filesystem'
     PERMANENT_SESSION_LIFETIME = timedelta(hours=1)
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = os.environ.get('FLASK_ENV', 'development') == 'production'
+    SESSION_COOKIE_SECURE = False  # Set to True in production with HTTPS
     SESSION_COOKIE_SAMESITE = 'Lax'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max file size
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER', 'static/images')
